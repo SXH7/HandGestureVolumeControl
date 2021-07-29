1aa73240a2e9a4c9ba8061e31e99dfbf726d6ce3
Shashwat Singh <70637141+SXH7@users.noreply.github.com>
2021-07-29 17:53:57 +0530

    Update VolumeHandControl.py

--- VolumeHandControl.py
+++ VolumeHandControl.py
@@ -3,6 +3,11 @@ import mediapipe as mp
 import numpy as np
 import time
 import handTrackingModule as htm
+import math
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+
 
 wcam, hcam = 640, 480
 
@@ -16,6 +21,24 @@ detector = htm.handDetector()
 
 
 
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(
+    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = cast(interface, POINTER(IAudioEndpointVolume))
+
+#volume.GetMute()
+#volume.GetMasterVolumeLevel()
+
+volRange = volume.GetVolumeRange()
+
+
+minVol = volRange[0]
+maxVol = volRange[1]
+
+
+
+
+
 while True:
 
     success, img = cap.read()
@@ -23,8 +46,31 @@ while True:
     img = detector.findHands(img)
     lmList = detector.findPosition(img, draw=False)
     if len(lmList) != 0:
-        print(lmList[8])
-        print(lmList[4])
+
+        x1, y1 = lmList[4][1], lmList[4][2]
+        x2, y2 = lmList[8][1], lmList[8][2]
+        cx, cy = (x1+x2)//2, (y1+y2)//2
+
+        cv.circle(img, (x1, y1), 15, (255, 0, 255), cv.FILLED)
+        cv.circle(img, (x2, y2), 15, (255, 0, 255), cv.FILLED)
+
+        cv.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+        cv.circle(img, (cx, cy), 15, (255, 0, 255), cv.FILLED)
+
+        length = math.hypot(x2-x1, y2-y1)
+        #print(length)
+
+        # hand range 20 200
+        # volume range -65 0
+
+        volumex = np.interp(length, [20, 200], [minVol, maxVol])
+        print(volumex)
+        volume.SetMasterVolumeLevel(volumex, None)
+
+
+
+        if (length<50):
+            cv.circle(img, (cx, cy), 15, (0, 255, 0), cv.FILLED)
 
     ctime = time.time()
     fps = 1/(ctime-ptime)
